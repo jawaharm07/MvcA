690f46e287ae3de79b4217860c1a253b7922b060
jawaharm07 <jawaharmayandi@gmail.comm>
2024-10-22 12:03:31 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcAPI.Models;
+using Newtonsoft.Json;
+
+namespace MvcAPI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7247/Api");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<ProductViewModel> productviewmodel = new List<ProductViewModel>();
+
+            try
+            {
+                // Since BaseAddress is set, use relative path
+                HttpResponseMessage response = await _httpClient.GetAsync("Product/Get");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await response.Content.ReadAsStringAsync();
+                    productviewmodel = JsonConvert.DeserializeObject<List<ProductViewModel>>(data);
+                }
+                else
+                {
+                    // Handle unsuccessful response
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (if logging is configured)
+                ModelState.AddModelError(string.Empty, "An error occurred while fetching the data.");
+            }
+
+            return View(productviewmodel);
+        }
+    }
+
+}
